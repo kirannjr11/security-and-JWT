0f263a517466c90e78121321e4188028dd83653c
kiran <ghalekiran03@gmail.com>
2024-08-17 01:36:40 +0900

    csrf created

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,14 +1,41 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Student;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 
 public class StudentController {
+
+    private List<Student> student = new ArrayList<>(List.of(
+            new Student(1, "kiran", 60),
+            new Student(2, "ghale", 80),
+            new Student(3, "lama", 90),
+            new Student(4, "gurung", 90)
+    ));
     @GetMapping ("/")
-    public String student(HttpServletRequest request) {
-        return "i am student" +" " +request.getSession().getId();
+    public List<Student> students() {
+        return student;
     }
+
+
+    @PostMapping("/")
+    public Student addStudent(@RequestBody Student students) {
+        student.add(students);
+        return students;
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getCsrftoken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
 }
