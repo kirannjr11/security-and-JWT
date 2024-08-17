0489544bb57a54a658a5710366b6494d9882b25a
kiran <ghalekiran03@gmail.com>
2024-08-17 21:57:22 +0900

    implemeted userDetail service

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String username;
+    private String password;
+}
